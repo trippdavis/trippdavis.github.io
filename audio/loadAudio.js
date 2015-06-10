56f6c411027bb6f65d93dcfa64629987f2f9001f
Tripp Davis <trippdavis3@gmail.com>
2015-06-10 16:48:28 -0700

    images and audio load files

---
+++ audio/loadAudio.js
@@ -0,0 +1,12 @@
+( function () {
+  window.refillSound = new Audio("audio/refill.wav");
+  window.backgroundMusic = new Audio("audio/contact.wav");
+  window.explosionSound = new Audio("audio/explosion.wav");
+  window.explosionSound.volume = 0.8;
+  window.gunshotSound = new Audio("audio/gunshot.wav");
+  window.gunshotSound.volume = 0.12;
+  window.shotgunSound = new Audio("audio/shotgun.wav");
+  window.shotgunSound.volume = 0.4;
+  window.laserSound = new Audio("audio/laser.wav");
+  window.laserSound.volume = 0.5;
+})();
---
+++ images/loadImages.js
@@ -0,0 +1,10 @@
+(function () {
+  window.spaceshipImg = new Image();
+  window.spaceshipImg.src = "images/spaceship.png";
+  window.spaceshipRecoilImg = new Image();
+  window.spaceshipRecoilImg.src = "images/spaceshipRecoil.png";
+  window.asteroidImg = new Image();
+  window.asteroidImg.src = "images/asteroid.png";
+  window.spaceshipSafeImg = new Image();
+  window.spaceshipSafeImg.src = "images/spaceshipSafe.png";
+})();
